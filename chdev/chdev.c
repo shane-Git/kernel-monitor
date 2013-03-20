75f51c122f8438678793865744d7754fb372aabf
shane <shane@shane-VirtualBox.(none)>
2013-03-20 13:51:14 +0800

    Add the instruction of mknod and change the init of sema

--- chdev/chdev.c
+++ chdev/chdev.c
@@ -7,14 +7,14 @@
 #define DEVICE_NAME "chdev"
 #define BUF_LEN 1024
 #define DEV_NO 66
+
 static int Device_Open = 0;
-static char devbuffer[BUF_LEN];
+static char devbuffer[BUF_LEN]="Hello!!!";
 struct semaphore mu;
 int i;
 
 static int device_open(struct inode *inode, struct file *file)
 {
-	sema_init(&mu,1);
 	Device_Open++;
 	return 0;
 }
@@ -37,17 +37,18 @@ static ssize_t device_read(struct file *filp,char *buffer,size_t length,loff_t *
 
 static ssize_t device_write(struct file *file,const char *buffer, size_t length, loff_t *offset)
 {
-	down_interruptible(&mu);
+//	down_interruptible(&mu);
 	for(i=0;i<BUF_LEN;i++)
 	{
 		devbuffer[i]=0;
 	}
 	if(copy_from_user(devbuffer,buffer,length))
 	{
+		printk("Write Function Encountered some error\n");
 		return 1;
 	}
-	up(&mu);
-	printk("Once write %d",length);
+//	up(&mu);
+	printk("Once write %ld",length);
 	return 0;
 } 
 
@@ -63,6 +64,8 @@ int init_module(void)
 {
 	register_chrdev(DEV_NO, DEVICE_NAME, &fops);
 	printk("Load chdev device into kernel.\n");
+	printk("Please use `sudo mknod chdev c 66 0`\nto create device\n");
+	sema_init(&mu,1);
 	return 0;
 }
 
